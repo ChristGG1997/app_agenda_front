53b4175130c151de29a772eeedfa063800a754d1
Christian Iglesias <59263708+ChristGG1997@users.noreply.github.com>
2024-04-18 08:39:33 -0500

    Initial commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home: React.FC = () => {
+    return (
+        <div className="home">
+            <h1 className='text-3xl font-bold underline'>Welcome to the Home Page</h1>
+        </div>
+    );
+}
+
+export default Home;
