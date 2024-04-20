155f223df4748c21de97b87f62a379fe32b41c6c
Christian Iglesias <59263708+ChristGG1997@users.noreply.github.com>
2024-04-20 18:09:24 -0500

    REFAC: Se cambio la estructura del home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
+import { DashboardProps } from '../../components/Dashboard/DashboardProps';
+import Dashboard from '../../components/Dashboard';
+import { Link } from 'react-router-dom';
 
-const Home: React.FC = () => {
-    return (
-        <div className="home">
-            <h1 className='text-3xl font-bold underline'>Welcome to the Home Page</h1>
-        </div>
-    );
+import { getContacts } from '../../services/contactService';
+
+const Home: React.FC<DashboardProps> = () => {
+
+  const [view, setView] = useState('list');
+
+  const [contacts, setContacts] = useState([
+    {
+      id: 0,
+      name: "",
+      phone: 0,
+      address: "",
+      prefix: 0,
+      email: "",
+      image: ""
+    },
+  ]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      await getContacts().then((response) => {        
+        setContacts(response);
+      }).catch((error: unknown) => {
+        console.error('Error:', error);
+      });
+    };
+
+    fetchContacts();
+  }, []);
+
+  return (
+    <Dashboard>
+      <h1 className='text-3xl font-bold underline'>Agenda de Contactos</h1>
+      <button onClick={() => setView(view === 'list' ? 'grid' : 'list')}>
+        Cambiar vista
+      </button>
+      <div className={view === 'list' ? 'flex flex-col' : 'grid grid-cols-2 gap-4'}>
+      {contacts && contacts.map((contact, index) => (
+  <div key={index} className='p-4 border rounded shadow'>
+    <h2 className='text-xl font-bold'>{contact.name}</h2>
+    <p>{contact.phone}</p>
+    <Link to={`/detalle/${contact.id}`}>Ver detalles</Link>
+  </div>
+))}
+      </div>
+    </Dashboard>
+  );
 }
 
 export default Home;
