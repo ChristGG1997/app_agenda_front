a15f30efb252d2462c1c79779dd8f240a8abee68
Christian Iglesias <59263708+ChristGG1997@users.noreply.github.com>
2024-04-20 22:58:58 -0500

    FIX: Se arreglo el funcionamiento de la peticion y el desiño

--- src/pages/Add/Add.tsx
+++ src/pages/Add/Add.tsx
@@ -2,79 +2,123 @@ import React, { useState } from 'react';
 import './Add.css';
 import { DashboardProps } from '../../components/Dashboard/DashboardProps';
 import Dashboard from '../../components/Dashboard';
+import ImageModal from '../../components/ImageModal';
+import { useNavigate } from 'react-router-dom';
 
-import { createContact } from '../../services/contactService'; 
+
+import { createContact } from '../../services/contactService';
 
 
-const Add: React.FC<DashboardProps>  = () => {
-    const [contact, setContact] = useState({
-        Name: '',
-        Phone: '',
-        Address: '',
-        Prefix: '',
-        Email: '',
-        Image: '',
+const Add: React.FC<DashboardProps> = () => {
+  const navigate = useNavigate();
+  const [contact, setContact] = useState({
+    name: '',
+    phone: '',
+    address: '',
+    prefix: '',
+    email: '',
+    image: '',
+  });
+  const [showModal, setShowModal] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContact(prevState => {
+      if (prevState) {
+        return { ...prevState, [e.target.name]: e.target.value };
+      } else {
+        return {
+          name: "",
+          phone: "",
+          address: "",
+          prefix: "",
+          email: "",
+          image: "",
+          imageUrl: "",
+          [e.target.name]: e.target.value,
+        };
+      }
+    });
+  };
+
+  const handleImageSelect = (imageName: string) => {
+    setContact(prevState => ({
+      ...prevState,
+      image: imageName,
+    }));
+    setShowModal(false);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (contact) {
+      await createContact(contact).then(() => {
+        console.log('Contacto creado');
+        navigate('/');
+      }).catch((error) => {
+        console.error('Error:', error);
       });
-    
-      const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setContact({ ...contact, [e.target.name]: e.target.value });
-      };
-    
-      const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        await createContact(contact).then(() => {
-          console.log('Contacto creado');
-          
-        }).catch((error) => {
-          console.error('Error:', error);
-        })
-      };
-    
-      return (
-        <Dashboard>
-          <h1 className='text-3xl font-bold underline'>Agregar Contacto</h1>
-          <form onSubmit={handleSubmit} className='space-y-4'>
-            <input
-              name='Name'
-              value={contact.Name}
-              onChange={handleChange}
-              placeholder='Nombre'
-              className='w-full p-2 border rounded'
-            />
-            <input
-              name='Phone'
-              value={contact.Phone}
-              onChange={handleChange}
-              placeholder='Teléfono'
-              className='w-full p-2 border rounded'
-            />
-            <input
-              name='Address'
-              value={contact.Address}
-              onChange={handleChange}
-              placeholder='Dirección'
-              className='w-full p-2 border rounded'
-            />
-            <input
-              name='Prefix'
-              value={contact.Prefix}
-              onChange={handleChange}
-              placeholder='Prefijo'
-              className='w-full p-2 border rounded'
-            />
-            <input
-              name='Email'
-              value={contact.Email}
-              onChange={handleChange}
-              placeholder='Email'
-              className='w-full p-2 border rounded'
-            />
-            <button type='submit' className='w-full p-2 bg-blue-500 text-white rounded'>
-              Guardar Contacto
-            </button>
-          </form>
-        </Dashboard>
-      );
+    } else {
+      console.error('Error: contact es undefined');
+    }
+  };
+
+  return (
+    <Dashboard>
+      <h1 className='text-3xl font-bold underline'>Agregar Contacto</h1>
+
+      <img
+        src={contact?.image ? require(`../../assets/svg/${contact.image}`) : require(`../../assets/images/default.png`)}
+        alt='Imagen del contacto'
+        className='w-48 h-48 my-16 object-cover mx-auto'
+        onClick={() => setShowModal(true)} // Abre el modal al hacer clic en la imagen
+      />
+      <ImageModal
+        isOpen={showModal}
+        onSelect={handleImageSelect}
+      />
+
+<form onSubmit={handleSubmit} className='space-y-4 w-full sm:w-full md:w-2/3 lg:w-1/2 xl:w-2/5 mx-auto flex flex-col items-center'>
+    <input
+    name='name'
+    value={contact?.name}
+    onChange={handleChange}
+    placeholder='Nombre'
+    className='w-full p-3 text-lg border rounded'
+  />
+  <input
+    name='phone'
+    value={contact?.phone}
+    onChange={handleChange}
+    placeholder='Teléfono'
+    className='w-full p-3 text-lg border rounded'
+  />
+  <input
+    name='address'
+    value={contact?.address}
+    onChange={handleChange}
+    placeholder='Dirección'
+    className='w-full p-3 text-lg border rounded'
+  />
+  <input
+    name='prefix'
+    value={contact?.prefix}
+    onChange={handleChange}
+    placeholder='Prefijo'
+    className='w-full p-3 text-lg border rounded'
+  />
+  <input
+    name='email'
+    value={contact?.email}
+    onChange={handleChange}
+    placeholder='Email'
+    className='w-full p-3 text-lg border rounded'
+  />
+  <button type='submit' className='w-full p-3 text-lg bg-blue-500 text-white rounded'>
+    Guardar Contacto
+  </button>
+</form>
+    </Dashboard>
+  );
 }
 
 export default Add;
